5b31ab90571741896206ecbf28fb734ccb0aeb7b
Ivan Stolov <ivan_stolov@mail.ru>
2022-12-24 15:22:05 +0300

    upd backup

--- backup.py
+++ backup.py
@@ -1,28 +1,40 @@
+from argparse import ArgumentParser
 import pandas as pd
 import mysql.connector
 import os
 from mysql.connector import errorcode
 from datetime import datetime
 
-import config_example
+import config
 
 
 file_name = f'backup_{datetime.now()}.csv'
+default_path = './'
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--path', required=False,
+                        type=str, help='path to directory where to save backup CSV')
+    args = parser.parse_args()
+    if args.path is None:
+        args.path = default_path
+    return args.path
 
 
 def establish_connection():
     return mysql.connector.connect(
-        host=config_example.host,
-        user=config_example.user,
-        password=config_example.password,
+        host=config.host,
+        user=config.user,
+        password=config.password,
     )
 
 
-def main(path_to_file = "./"):
+def main(path_to_file = default_path):
     try:
         connection = establish_connection()
         df = pd.read_sql('select * from db_django.app_solutions', con=connection)
-        path = os.join(path_to_file, file_name)
+        path = os.path.join(path_to_file, file_name)
         df.to_csv(path)
         print(f'Saved backup to {path}')
     except mysql.connector.Error as err:
@@ -39,4 +51,8 @@ def main(path_to_file = "./"):
 
 
 if __name__ == '__main__':
-    main()
+    path = parse_args()
+    if os.path.isdir(path):
+        main(path)
+    else:
+        print(f'ERROR: Directory {path} not found')
